5b55a212adb15308008e6e275675ffd206d86ee5
Cruzverme <grossmino@hotmail.com>
2019-07-09 22:57:14 -0300

    Add Dependent Destroy

--- app/models/campaign_client.rb
+++ app/models/campaign_client.rb
@@ -2,6 +2,6 @@ class CampaignClient < ApplicationRecord
   belongs_to :campaign
   belongs_to :client
 
-  has_many :campaign_clients
+  has_many :campaign_clients, dependent: :destroy
   has_many :clients, through: :campaign_clients
 end
