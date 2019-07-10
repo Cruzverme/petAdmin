35116b895207c5b9a943c36c47166c46e604781b
Cruzverme <grossmino@hotmail.com>
2019-07-09 22:15:31 -0300

    Add SetTotal

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -12,6 +12,8 @@ class Sell < ApplicationRecord
   
   has_many :sell_services
   has_many :services, through: :sell_services
+
+  before_save :set_total
  
   def fae_display_field
     id
@@ -19,6 +21,21 @@ class Sell < ApplicationRecord
  
   def self.for_fae_index
     order(:id)
+  end
+
+  private 
+
+  def set_total 
+    total = 0
+    self.products.each { |p| total += p.price }
+    self.services.each { |s| total += s.price }
+
+    if self.discount.present?
+      total -= self.discount.value
+    end
+
+    total = (total >= 0)? total : 0;
+    self.total = total
   end
  
 
